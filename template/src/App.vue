e20c5d393c002cca74b1661591cea6dcff6e79be
lulichn <daisuke.develop@gmail.com>
2018-03-29 12:59:53 +0900

    simple vue template

--- template/src/App.vue
+++ template/src/App.vue
@@ -13,7 +13,6 @@
     <ul>
       <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
       <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
     </ul>
   </div>
@@ -30,7 +29,7 @@ export default {
 }
 </script>
 
-<style{{#sass}} lang="scss"{{/sass}}>
+<style>
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
